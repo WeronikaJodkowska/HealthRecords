70329ec90adcbd01617a2cf313d67f72dd719fbe
Weronika Jodkowska <veronica.jodkowska@gmail.com>
2022-11-07 19:38:36 +0300

    users app added

--- HealthRecords/settings.py
+++ HealthRecords/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'HealthRecords.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'health',
+        'USER': 'health',
+        'PASSWORD': 'health',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
