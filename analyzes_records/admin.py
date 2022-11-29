6de35ed817f744723f711dc502cbda940683635f
veronica.jodkowska <veronica.jodkowska@gmail.com>
2022-11-29 15:36:24 -0800

    Test models

--- analyzes_records/admin.py
+++ analyzes_records/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from easy_select2 import select2_modelform
 
-# Register your models here.
+from .models import LabTest, Test
+
+
+LabTestForm = select2_modelform(LabTest, attrs={"width": "250px"})
+TestForm = select2_modelform(Test, attrs={"width": "250px"})
+
+
+@admin.register(LabTest)
+class LabTestAdmin(admin.ModelAdmin):
+    form = LabTestForm
+    list_display = ["user", "test_date"]
+
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    form = TestForm
+    list_display = ["analysis", "lab_test_id"]
--- analyzes_records/models.py
+++ analyzes_records/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from reference_information.models import Analysis, Laboratory
+
+
+class LabTest(models.Model):
+    user = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
+    lab = models.ForeignKey(Laboratory, default=None, on_delete=models.CASCADE)
+    test_date = models.DateField()
+    test_time = models.TimeField(blank=True, null=True)
+    previous_test = models.ForeignKey(
+        "self", null=True, blank=True, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return (
+            self.user.email
+            + " / "
+            + self.lab.title
+            + " / "
+            + str(self.test_date)
+        )
+
+
+class Test(models.Model):
+    analysis = models.ForeignKey(Analysis, on_delete=models.CASCADE)
+    result = models.FloatField()
+    measurement = models.CharField(max_length=50)
+    reference = models.TextField(blank=True, null=True)
+    comment = models.TextField(blank=True, null=True)
+    lab_test_id = models.ForeignKey(LabTest, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (
+            self.analysis.title
+            + " / "
+            + self.lab_test_id.lab.title
+            + " / "
+            + str(self.lab_test_id.test_date)
+        )
